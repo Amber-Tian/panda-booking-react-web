6b57dabf2e3fd0ca27a414bd1714dc9bd75458a3
Amber <649458276@qq.com>
2020-09-19 11:21:38 +0800

    deploy

---
+++ precache-manifest.0ae19c8b87ebba3681dd32a4f0d1d68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3800650376a2d18c07c2cc0fba5c13aa",
+    "url": "/panda-booking-react-web/index.html"
+  },
+  {
+    "revision": "f71bceadeb0826cdfd12",
+    "url": "/panda-booking-react-web/static/css/main.becdf685.chunk.css"
+  },
+  {
+    "revision": "3de0ca835604158259d6",
+    "url": "/panda-booking-react-web/static/js/2.caad8dee.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/panda-booking-react-web/static/js/2.caad8dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71bceadeb0826cdfd12",
+    "url": "/panda-booking-react-web/static/js/main.68025d01.chunk.js"
+  },
+  {
+    "revision": "d1a0b3275f30faff3bf2",
+    "url": "/panda-booking-react-web/static/js/runtime-main.2809a574.js"
+  }
+]);
